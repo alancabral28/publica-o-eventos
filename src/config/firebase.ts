9749f290d0bd399d7a01cc502b7178d28e32f43e
alancabral <alanfab28@gmail.com>
2022-02-26 13:01:12 -0300

    primeira correção autenticação firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBIT2SXlqZbdlmHDBjRymxTY-wi2QaUrWg",
+    authDomain: "eventos-vasco-cinquini.firebaseapp.com",
+    projectId: "eventos-vasco-cinquini",
+    storageBucket: "eventos-vasco-cinquini.appspot.com",
+    messagingSenderId: "846481150208",
+    appId: "1:846481150208:web:f3f1532ffe69e8c219e3e5"
+  };
+  
+  // Initialize Firebase
+   const app = initializeApp(firebaseConfig);
+
+   export default app;
+ 
+
+
