0c7d84b59b4d5d1e6d294a62f9f6a4e924c12d0d
alancabral <alanfab28@gmail.com>
2022-03-28 10:17:40 -0300

    tela de login e começo tela de cadastro 3

--- src/view/evento-cadastro/index.tsx
+++ src/view/evento-cadastro/index.tsx
@@ -7,7 +7,7 @@ import Navbar from '../../components/navbar';
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc} from "firebase/firestore";
 import { getApp } from "firebase/app";
-import { getStorage, ref} from "firebase/storage";
+import { getStorage, ref, updateMetadata, uploadBytes, uploadString} from "firebase/storage";
 import { getDatabase } from 'firebase/database';
 import 'firebase/firestore';
 
@@ -15,12 +15,12 @@ import 'firebase/firestore';
 function EventoCadastro(): JSX.Element {
 
     const [msgTipo, setMsgTipo] = useState('');
-    const [foto, setFoto] = useState();
-    const [titulo, setTitulo] = useState();
-    const [tipo, setTipo] = useState();
-    const [detalhes, setDetalhes] = useState();
-    const [data, setData] = useState();
-    const [hora, setHora] = useState();    
+    const [foto, setFoto] = useState('');
+    const [titulo, setTitulo] = useState('');
+    const [tipo, setTipo] = useState('');
+    const [detalhes, setDetalhes] = useState('');
+    const [data, setData] = useState('');
+    const [hora, setHora] = useState('');    
     const [fotoAtual, setFotoAtual] = useState();    
     const [fotoNova, setFotoNova] = useState();    
     const [usuarioEmail, setUsuarioEmail] = useState();
@@ -29,11 +29,14 @@ function EventoCadastro(): JSX.Element {
 
     const storage = getStorage();
     const db = getFirestore();
+    const storageRef = ref(storage);
 
-    setUsuarioEmail(useSelector((state:any) => state.usuarioEmail));
+  
 
     function cadastrar() {
-        const imagesRef = ref(storage, 'imagens/${foto.name}').put(foto).then(() => {
+        setMsgTipo('');
+        const imagesRef = ref(storage, 'imagens/${foto.name}');
+        uploadString(storageRef, foto).then(() => {
             try {
                 const docRef =  addDoc(collection(db, "eventos"), {
                     titulo: titulo,
@@ -46,10 +49,14 @@ function EventoCadastro(): JSX.Element {
                     foto:foto,
                     publico:1,
                     criacao:new Date ()
-                });
-              } catch (e) {
-                console.error("Não foi possível cadastrar o evento! ", e);
-              }
+
+                }).then (() => {
+                    setMsgTipo('sucesso');
+                })
+
+              } catch (erro) {
+                setMsgTipo('erro');
+            }
         })
     }
 
@@ -64,12 +71,12 @@ function EventoCadastro(): JSX.Element {
             <form>
                 <div className="form-group mb-4">
                     <label>Título:</label>
-                    <input type="text" className="form-control" />
+                    <input onChange={(e) => setTitulo(e.target.value)} type="text" className="form-control" />
                 </div>
 
                 <div className="form-group mb-4">
                     <label>Tipo de Evento:</label>
-                    <select className="form-control">
+                    <select onChange={(e) => setTipo(e.target.value)} className="form-control">
                         <option disabled selected>-- Selecione um tipo --</option>
                         <option>Festa</option>
                         <option>Teatro</option>
@@ -79,23 +86,23 @@ function EventoCadastro(): JSX.Element {
                 </div>
                 <div className="form-group mb-4">
                     <label>Descrição do Evento:</label>
-                    <textarea className="form-control" rows="3"/>
+                    <textarea onChange={(e) => setDetalhes(e.target.value)} className="form-control" rows="3"/>
                 </div>
 
                 <div className="form-group row mb-4">
                     <div className="col-6">
                         <label>Data:</label>
-                        <input type="date" className="form-control"/>
+                        <input onChange={(e) => setData(e.target.value)} type="date" className="form-control"/>
                     </div>
                     <div className="col-6">
                         <label>Hora:</label>
-                        <input type="time" className="form-control"/>
+                        <input  onChange={(e) => setHora(e.target.value)}type="time" className="form-control"/>
                     </div>
                 </div>
 
                 <div className="form-group">
                     <label>Upload da Foto:</label>
-                    <input type="file" className="form-control" />
+                    <input onChange={(e) => setFoto(e.target.files[0])} type="file" className="form-control" />
                 </div>
                 <div className="row">
                     <button onClick={cadastrar} type="button" className="btn btn-lg btn-block mt-3 mb-5 btn-cadastro">Publicar Evento</button>
